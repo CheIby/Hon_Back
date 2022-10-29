9d4cf86399934388f3554b12edbcd1132e540be9
CheIby <ootcazy007@gmail.com>
2022-10-29 18:15:22 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const msgModel = require('./model/msgSchema')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017')
+.then(()=>console.log("Connect DB Success"))
+.catch(()=>console.log("Fucking Fail Connect"))
+
+app.use(cors())
+app.use(express.json())
+app.post('/post',async(req,res)=>{
+    const newMsg = await new msgModel({msg:req.body.msg})
+    newMsg.save()
+    res.json(newMsg)
+})
+app.get('/getall',async (req,res)=>{
+    const allMsg = await msgModel.find()
+    res.json(allMsg)
+})
+
+app.listen(process.env.PORT || 7000,() =>{
+    console.log("Backend running")
+})
